523b16a6dc4ec07768ba922dce462d498e15aee0
InaProkhorovaDunice <e.prohorova@dunice.net>
2021-03-19 16:51:39 +0300

    add router, hooks and base structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './components/common/header';
+import MainPage from './components/mainPage';
 
 function App() {
-  return <div className="App">23432424</div>;
+  return (
+    <div className="App">
+      <Router basename="/app">
+        <Header />
+        <div className={'main-content'}>
+          <Switch>
+            <Route exact path="/" component={MainPage} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/mainPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MainPage = () => {
+  return <div>65156156</div>;
+};
+
+export default React.memo(MainPage);
